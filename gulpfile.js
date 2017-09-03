28ac43a3ded4aaa78eb921a774f59cee6bbd86eb
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-09-03 12:05:59 -0300

    Concatenates pure css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 var gulp = require("gulp");
 var ugligy = require("gulp-uglify");
 var livereload = require("gulp-livereload");
+var concat = require("gulp-concat");
 
 // FOLDER'S PATHS
 const   SCRIPTS_PATH = "public/js/**/*.js",
+        CSS_PATH = "public/styles/**/*.css",
         DIST_PATH = "dist";
 
 // STYLE PROCESS
 gulp.task("styles", function() {
-
+    return gulp.src(CSS_PATH)
+                .pipe(concat("app.css"))
+                .pipe(gulp.dest(DIST_PATH+"/styles"))
+                .pipe(livereload());
 });
 
 // SCRIPTS PROCESS
@@ -24,7 +29,8 @@ gulp.task("watch",function() {
     console.log("Starting watch...");
     require("./server.js");
     livereload.listen();
-    gulp.watch(SCRIPTS_PATH, ["scripts"])
+    gulp.watch(SCRIPTS_PATH, ["scripts"]);
+    gulp.watch(CSS_PATH, ["styles"]);
 });
 
 // DEFAULT TASK
