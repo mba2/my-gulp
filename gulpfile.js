37b0671e197e0a0b4972c50f55da974583351762
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-09-09 13:22:18 -0300

    Adds an basic autoprefixer task

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
-var gulp        = require("gulp");
-var ugligy      = require("gulp-uglify");
-var livereload  = require("gulp-livereload");
-var concat      = require("gulp-concat");
-var minifyCSS   = require("gulp-minify-css");
+var gulp            = require("gulp");
+var ugligy          = require("gulp-uglify");
+var livereload      = require("gulp-livereload");
+var concat          = require("gulp-concat");
+var minifyCSS       = require("gulp-minify-css");
+var autoprefixer    = require("gulp-autoprefixer");
 
 // FOLDER'S PATHS
-const   SCRIPTS_PATH = "public/js/**/*.js",
-        CSS_PATH = "public/styles/**/*.css",
-        DIST_PATH = "dist";
+const   SCRIPTS_PATH    = "public/js/**/*.js",
+        CSS_PATH        = "public/styles/**/*.css",
+        DIST_PATH       = "dist";
 
 // STYLE PROCESS
 gulp.task("styles", function() {
     return gulp.src(CSS_PATH)
                 .pipe(concat("app.css"))                // CONCATENATES ALL PURE CSS INTO A UNIQUE FILE 
                 .pipe(minifyCSS())                      // MINFY THIS UNIQUE FILE
+                .pipe(autoprefixer())                   // INSERT VENDOR PREFIXES
                 .pipe(gulp.dest(DIST_PATH+"/styles"))
                 .pipe(livereload());
 });
