bb95bba68e2f6b112e0c055e255f70700286e747
Mario B. Almeida <mariobrusarosco>
2017-10-26 19:26:25 -0200

    Clean task for production and development environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,42 @@
-var gulp            = require("gulp");
+// MODULES
+// =========================
+const gulp          = require('gulp');                  // MAIN MODULE
+const $             = require('gulp-load-plugins')();   // THIS VARIABLE WILL HOLD ALMOST !!! ALL PLUGINS
+
+// MODULES THAT FAILS TO BE LOADED INTO $ VARIABLE
+// =============== 
+const del = require('del');                             // MODULE TO DELETE FILES AND FOLDERS
+
+// BUILD CONFIGURATION OPTIONS
+// =============== 
+const SRC_PATH      =  'public';
+const DEV_PATH      =  'build';
+const DEPLOY_PATH   =  'deploy';
+const PROD_ENV      =  !!$.util.env.production;
+
+
+
+// CLEAR PROCESS
+gulp.task('clean',() => {
+    return del(
+        (PROD_ENV) ? DEPLOY_PATH : DEV_PATH 
+    );
+});
+
+// STYLES PROCESS
+gulp.task('styles', () => {
+    
+});
+
+
 
 // DEFAULT TASK
 gulp.task("default",[
+                        'clean',
+                        'styles'
+                    ],
 
-    ],
-    function() {
-        
-    }
+                    () => {
+                    
+                    }
 );
